0202cb5d908ca881d176b2320bedc5f3018084ec
xiezs <xiezs@192.168.55.20>
2018-05-30 15:33:53 +0800

    backup code

---
+++ webmagic-app/src/main/java/com/imolihua/webmagic_app/App.java
@@ -0,0 +1,13 @@
+package com.imolihua.webmagic_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
